e1db259e864cbef090c587c0107c24ed7ca8727d
eunbinnie <dmsqlsdl54@gmail.com>
2024-09-14 01:39:59 +0900

    feat: favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Background from '@/components/StarContainer';
 export const metadata: Metadata = {
   title: '이은빈 | 프론트엔드 개발자 포트폴리오',
   description: '신입 프론트엔드 개발자 이은빈의 포트폴리오입니다.',
+  icons: '/favicon.ico',
 };
 
 export default function RootLayout({
