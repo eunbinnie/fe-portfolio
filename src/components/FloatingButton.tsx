041dbc0d9c30d3ac2696ac02fd6a7667c4231fc9
eunbinnie <dmsqlsdl54@gmail.com>
2024-09-24 00:12:02 +0900

    refactor: floating button 아이콘 변경

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -7,12 +7,12 @@ const FloatingButton = () => {
       className="fixed bottom-4 right-4 size-14 cursor-pointer rounded-full bg-silver shadow-small transition-colors duration-300 ease-in-out hover:bg-white md:size-16"
     >
       <Image
-        src="/icons/chatbot.svg"
+        src="/icons/chatgpt.svg"
         alt="AI와 채팅하기"
         fill
         sizes="max-width:100%"
         priority
-        className="object-cover p-3"
+        className="object-cover p-4"
       />
     </button>
   );
