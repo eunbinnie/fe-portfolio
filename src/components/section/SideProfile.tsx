6b1dc6d6b87a5ed50e5717dba4d73fa009070c4d
eunbinnie <dmsqlsdl54@gmail.com>
2024-11-26 18:04:02 +0900

    refactor: 이력서 pdf 파일 다운로드되도록 수정

--- src/components/section/SideProfile.tsx
+++ src/components/section/SideProfile.tsx
@@ -76,7 +76,7 @@ const SideProfile = () => {
             </Link>
             <a
               href="/files/이은빈_프론트엔드_이력서.pdf"
-              target="_blank"
+              download
               type="application/pdf"
               className="relative size-[18px]"
             >
