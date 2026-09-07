1482c6002b661e75ea6bf20ff7e336d9128ef9e4
eunbinnie <dmsqlsdl54@gmail.com>
2026-09-07 17:11:34 +0900

    refactor: 학력 및 경력 정렬을 subgrid로 변경

--- src/components/resume/CareerHistory.tsx
+++ src/components/resume/CareerHistory.tsx
@@ -2,14 +2,14 @@ import CAREER_HISTORY from '@/constants/careerHistory';
 
 const CareerHistory = () => {
   return (
-    <ul className="grid gap-5 sm:gap-1">
+    <ul className="grid gap-5 sm:grid-cols-[max-content_1fr] sm:gap-x-5 sm:gap-y-1">
       {CAREER_HISTORY.map((data, idx) => (
         <li
           key={idx}
-          className="flex flex-col gap-1 text-sm sm:flex-row sm:gap-5"
+          className="flex flex-col gap-2 text-sm sm:col-span-2 sm:grid sm:grid-cols-subgrid"
         >
-          <span className="text-silver">{data.period}</span>
-          <span className="flex-1 font-medium text-white">{data.title}</span>
+          <span className="tabular-nums text-silver">{data.period}</span>
+          <span className="font-medium text-white">{data.title}</span>
         </li>
       ))}
     </ul>
